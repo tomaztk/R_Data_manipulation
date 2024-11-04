09bcaf11a005776175e0aee9f8ca1c79360e7ad6
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2024-11-04 22:52:09 +0100

    update

---
+++ Session02/200_4_Dplyr_Join.R
@@ -0,0 +1,7 @@
+# county level data
+demo <- read_csv("https://raw.githubusercontent.com/nuitrcs/r-tidyverse/main/data/county_demographics.csv")
+cases <- read_csv("https://raw.githubusercontent.com/nuitrcs/r-tidyverse/main/data/county_cases.csv")
+deaths <- read_csv("https://raw.githubusercontent.com/nuitrcs/r-tidyverse/main/data/county_deaths.csv")
+
+# state level data
+state_stats <- read_csv("https://raw.githubusercontent.com/nuitrcs/r-tidyverse/main/data/state_covid.csv")
