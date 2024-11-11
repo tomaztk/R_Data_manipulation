e92a5cd67994cb49bf4994e97aac2bca6cfe9951
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2024-11-11 21:15:26 +0100

    updae

--- Session02/200_A_1_Dplyr.R
+++ Session02/200_A_1_Dplyr.R
@@ -38,3 +38,31 @@ dplyr::slice(iris, 10:15)
 
 # Select and order top n entries (by group if grouped data).
 dplyr::top_n(iris, 2, Species)
+
+
+
+### Summarising data
+# Summarise uses summary functions, functions that
+# take a vector of values and return a single value
+
+#  Summarise data into single row of values.
+dplyr::summarise(iris, avg = mean(Sepal.Length))
+
+# Apply summary function to each column.
+dplyr::summarise_each(iris, funs(mean))
+
+# Count number of rows with each unique value of variable (with or without weights).
+dplyr::count(iris, Species, wt = Sepal.Length)
+dplyr::count(iris, Species)
+
+
+dplyr::first(iris)
+dplyr::last(iris)
+dplyr::nth(iris,34)
+dplyr::n_distinct(iris)
+dplyr::summarise(iris, minimum=min(Sepal.Length))
+
+
+
+
+
