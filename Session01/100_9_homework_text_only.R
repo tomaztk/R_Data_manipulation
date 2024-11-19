66bf3f152faf2004a82a2f729a4610d6466203c9
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2024-11-19 19:02:27 +0100

    update

--- Session01/100_9_homework_text_only.R
+++ Session01/100_9_homework_text_only.R
@@ -21,36 +21,130 @@ rm(list = ls())
 # 4. Print all variables (a, b, c, and d).
 
 
+a <- 10
+b <- 20
+c <- a+b
+d = c/2
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+print(c(a,b))
+
 # ======================================
 # Task 2: Variable Assignment and Basic Arithmetic
 # ======================================
 
-# 1. Create a variable num_var with a numeric value, char_var with a character string, and bool_var with a logical (TRUE/FALSE) value.
-# 2. Use the class() function to check the data types of each variable (num_var, char_var, bool_var).
+# 1. Create a variable num_var with a numeric value, char_var 
+#    with a character string, and bool_var with a logical (TRUE/FALSE) value.
+# 2. Use the class() function to check the data types of each 
+#    variable (num_var, char_var, bool_var).
 # 3. Convert num_var to character and store it in a new variable num_char.
-# 4. Print the data type of num_char to confirm the conversion. And check the class() function for the num_char variable.
+# 4. Print the data type of num_char to confirm the conversion. 
+#   And check the class() function for the num_char variable.
+
+num_var <- 10
+char_var <- "Banka"
+bool_var <- TRUE
+
+
+class(char_var)
+class(num_var)
+
+num_char <- as.character(num_var)
+print(num_var)
+
+kontra <- as.numeric(num_char)
+print(kontra)
+
+class(kontra)
 
 
 # ======================================
 #Task 3: Working with Vectors
 # ======================================
 
-# 1. Create a numeric vector num_vec with values 2, 4, 6, 8, 10. Create this vector in two different ways.
-# 2. Access the third element in num_vec and assign it to a variable third_elem.
-# 3. Calculate the sum of all elements in num_vec using the sum() function. Extra points: write your own function using loop
-# 4. Add a new element 12 to num_vec using c() and print the updated vector.
+# 1. Create a numeric vector num_vec with values 
+#     2, 4, 6, 8, 10. Create this vector in two different ways.
+# 2. Access the third element in num_vec and assign
+#     it to a variable third_elem.
+# 3. Calculate the sum of all elements in num_vec 
+#    using the sum() function. 
+#  Extra points: write your own function using loop
+# 4. Add a new element 12 to num_vec using c() 
+#    and print the updated vector.
+
+num_vec <- c(2, 4, 6, 8, 10)
+num_vec2 <- seq(from = 2, to = 10,  by = 2)
+num_vec2A <- seq(2,10,2)
+num_vec2A <- seq(2,   10,    2)
+
+element1 <- 12
+num_vec <- c(num_vec, element1)
+
+sum(num_vec, element1)
+
+
+sum_var <- 0
+
+for(i in 1:length(num_vec)){
+  sum_var <- sum_var + num_vec[i]
+  }
+
+print(sum_var)
+
+
 
 
 # ======================================
 # Task 4: Matrix Creation and Basic Operations
 # ======================================
 
-# 1. Create a 3x3 matrix mat with values from 1 to 9 using the matrix() function and orientation by rows.
-# 2. Calculate the row-wise sum and column-wise sum of mat using rowSums() and colSums(). 
-#     Extra points: write your own function for sum of columns or rows.
+# 1. Create a 3x3 matrix mat with values from 1 to 9 
+# using the matrix() function and orientation by rows.
+# 2. Calculate the row-wise sum and column-wise sum of
+#    mat using rowSums() and colSums(). 
+#     Extra points: write your own function for sum of
+#     columns or rows.
 # 3. Access the element in the second row and third column and store it in matrix_el23.
 # 4. Multiply each element in the matrix by 2 and print the updated matrix.
 #     Extra points:  Multiply the matrix with the vector 2,4,6.
+
+
+1:9
+
+matrika <- matrix(1:9, nrow=3, ncol=3, byrow=TRUE)
+
+matrika
+rowSums(matrika)
+colSums(matrika)
+
+
+rowsums(matrika)
+
+#Access the element in the second row 
+#and third column and store it in matrix_el23.
+
+matrix_el23 <- matrika[2,3]
+print(matrix_el23)
+
+update_matrika <- matrika*2
+print(update_matrika)
+
+
+update_matrika<- matrika[2,3] * 2
+print(update_matrika)
+
+dim(matrika)
+
+# multiply  position 2_3 in 3x3 matrix with value 2
+for (i in 1:3) {
+  for (j in 1:4){
+    print (i)  
+  }
+}
 
 
 # ======================================
@@ -76,6 +170,44 @@ df <- data.frame(
   Starost = c(30,40,35,42,32),
   Kredit_sposob = c(0.32, 0.54, 0.66, 0.52, 0.51)
 )
+
+head(df, 3)
+tail(df, 1)
+
+df_subset <- df[  , c("Ime","Kredit_sposob") ]
+
+df_subset
+
+# 4. Extract and print the value from 
+# original df from column  'Starost' of the third person.
+
+df[ 3 , c("Starost") ]
+
+df[ df$Ime=="Tomaz" , c("Starost") ]
+
+# Select all the dows where 'Kredit_sposob' is equal
+# or bigger than 0.52  and the name ('Ime') starts with "Ti" and 
+#   return only  columns 'Ime' and 'Starost' 
+# and create a new dataframe named df_subset2
+
+help("substr")
+# substr(x, start, stop)
+
+df_subset2 <- df[ df$Kredit_sposob >= 0.52 & substr(df$Ime, 1,2) == "Ti", 
+                  c("Ime","Starost") ]
+
+df
+df_subset2
+
+
+a = 12
+class(a)
+
+a == a
+
+
+
+
 
 
 # ======================================
