c8df93bc5ee81d144c0e0230f5a9b691db5e2159
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2025-01-11 20:05:34 +0100

    update

--- Session03/300_0_Dplyr.R
+++ Session03/300_0_Dplyr.R
@@ -61,7 +61,78 @@ ggplot(mtcars_long, aes(x = car_name, y = value, fill = car_attribute)) +
 # Transforms data from long format to wide format.
 # Replacement for reshape() in Base R.
 
-head(billboard) #check how dataset looks
+mtcars_wide <- mtcars %>%
+  mutate(car_name = rownames(mtcars)) %>%
+  pivot_wider(
+      names_from = car_name, 
+      values_from = c(mpg,cyl,disp)
+    )
 
-billboard_wide <- billboard %>%
-  pivot_wider(names_from = week, values_from = rank)
+colnames(mtcars_wide)
+
+# example 2
+# simulating one-hot encoding
+mtcars %>%
+  mutate(car_name = rownames(mtcars)) %>%
+  pivot_wider(
+    names_from = cyl, # Column to spread 
+    values_from = mpg, 
+    names_prefix = "cyl_"
+  )
+
+# example 3
+mtcars %>%
+  mutate(car_name = rownames(mtcars)) %>%
+  group_by(gear, am, cyl) %>% 
+  #summarize(avg_mpg = mean(mpg), .groups = "drop") %>% # Calculate average mpg
+  pivot_wider(
+    names_from = cyl,        # Spread by the `cyl` column
+    # values_from = avg_mpg,   # Fill values with the `avg_mpg` column
+    values_fn = ~ mean(.x, na.rm = TRUE)
+    names_prefix = "cyl_"    # Add a prefix for clarity
+  )
+
+
+#example 4
+
+set.seed(2908)
+random_data <- data.frame(
+  country = rep(c("Slovenia", "Austria", "Portugal", "Germany", "Japan"), each = 5),
+  year = rep(2015:2019, times = 5),
+  population = rep(c(320, 1300, 200, 80, 125), each = 5) + sample(-10:10, 25, replace = TRUE), 
+  cases = sample(5000:50000, 25, replace = TRUE)
+)
+
+head(random_data,10)
+
+# wider dataset with year but 
+# still not 5x5 table, because of population
+random_data %>%
+  pivot_wider(
+    names_from = year,    
+    values_from = cases,  
+    names_prefix = "year_" #add prefix
+  )
+
+# do this
+random_data %>%
+  group_by(country) %>% 
+  mutate(avg_population = mean(population)) %>% 
+  select(country,avg_population,year,cases) %>%
+  pivot_wider(
+    names_from = year,
+    values_from = cases,
+    names_prefix = "cases_" #add prefix       
+  )
+
+# or
+random_data %>%
+  group_by(country) %>% 
+  mutate(avg_population = mean(population)) %>%   
+  ungroup() %>%
+  select(-population) %>%                           
+  pivot_wider(
+    names_from = year,                              
+    values_from = cases,                          
+    names_prefix = "cases_"                         
+  )
