f18ccc4a09bec6bc55cb9beeb858672054ab2696
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2025-01-12 22:06:30 +0100

    adding packages

--- Session03/300_1_Dplyr.R
+++ Session03/300_1_Dplyr.R
@@ -1,2 +1,53 @@
 ### Using Tidyverse - Part 5
-### exploring packages:  reader, purr, lubridate, ...
+### exploring packages:  ReadR, purrr,  DataEditR, gt
+
+
+
+## ## ## ## ## ## ## ## ## ## ## ##
+## readr (from Tidyverse)
+## ## ## ## ## ## ## ## ## ## ## ##
+# install.packages("readr")
+library(readr)
+
+
+
+## ## ## ## ## ## ## ## ## ## ## ##
+## purrr (from Tidyverse)
+## ## ## ## ## ## ## ## ## ## ## ##
+# install.packages("purrr")
+library(purrr)
+
+
+
+
+
+
+## ## ## ## ## ## ## ## ## ## ## ##
+## DataEditR
+## ## ## ## ## ## ## ## ## ## ## ##
+
+# install.packages("DataEditR")
+library(DataEditR)
+
+
+
+## ## ## ## ## ## ## ## ## ## ## ##
+## gt
+## ## ## ## ## ## ## ## ## ## ## ##
+# install.packages("gt")
+library(gt)
+
+start_date <- "2010-06-07"
+end_date <- "2010-06-14"
+
+sp500 |>
+  dplyr::filter(date >= start_date & date <= end_date) |>
+  dplyr::select(-adj_close) |>
+  gt() |>
+  tab_header(
+    title = "S&P 500",
+    subtitle = glue::glue("{start_date} to {end_date}")
+  ) |>
+  fmt_currency() |>
+  fmt_date(columns = date, date_style = "wd_m_day_year") |>
+  fmt_number(columns = volume, suffixing = TRUE)
