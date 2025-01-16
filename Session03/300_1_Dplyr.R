e1d41ff222c62e190eb0894cf424295707ac1ac6
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2025-01-16 07:45:07 +0100

    update

--- Session03/300_1_Dplyr.R
+++ Session03/300_1_Dplyr.R
@@ -9,9 +9,6 @@
 # install.packages("readr")
 library(readr)
 
-
-library(readr)
-
 # read_csv(): comma-separated values (CSV)
 # read_tsv(): tab-separated values (TSV)
 # read_csv2(): semicolon-separated values with , as the decimal mark
@@ -52,24 +49,47 @@ library(purrr)
 
 
 # The purrr package simplifies iterations, replacing for loops with elegant, functional-style code.
-# Key Functions:
+# Dplyr (from Tidyverse) works amazing with data.frames, purrr workhorse is list. List is  way of storing many objects of 
+#  any type (e.g. data frames, plots, vectors) together in a single object
+
+# Example of list, that has three elements: a single number, a vector and a data frame
+
+my_list <- list(my_num <- 2908 ,
+            num_vector <- c(1:10),
+            char_vector <- c("To", "je stavek. Razbit na vec elementov.", "ok", "d"),
+            my_df <- data.frame(A = 1:3, B = 4:6, C = 7:9),
+            my_mtcars <- as.data.frame(mtcars[1:10, c("mpg", "hp", "cyl")])
+    )                        
+
+#complete list
+my_list
+
+# 4th element of the list
+my_list[4]
+
+my_list$my_df[, 2]
+
+ 
+
+# Key Functions is map.
+# similar to R: apply
+
+# A map function is one that applies the same action/function to every element 
+# of an object (e.g. each entry of a list or a vector, or each of the columns of a data frame).
   
 # map(): Apply a function to each element of a list.
 # map_df(): Apply a function and return a data frame.
+# map_dbl(): Apply a function and return a vector of type double.
 # map2(): Map a function over two lists simultaneously.
 # pmap(): Map a function over multiple lists.
-
 
 
 tiger <- list(species = "tiger", name = "Cuddle", weight = 150, age = 12)
 shark <- list(species = "shark", name = "Teeth", weight = 302, age = 15)
 dragon <- list(species = "dragon", name = "Firebreath", weight = 2400, age = 4)
 
-# Merge them together
 animals <- list(tiger,shark,dragon)
 names(animals) <- c("earth","water","fantasy")
-
-# Now use map() to access the name of each: 
 
 map(animals,"name") # See output
 map(animals, "species") # See output
@@ -77,28 +97,31 @@ map(animals, "age") # See output. Cool!
 
 
 
-my_first_list <- list(my_number = 5,
-                      my_vector = c("a", "b", "c"),
-                      my_dataframe = data.frame(a = 1:3, b = c("q", "b", "z"), c = c("bananas", "are", "so very great")))
-my_first_list
+# !!! Input for map() can be: 1) a list 2) a vector or 3) a data.frame.
+# map() functino works great with %>% operator. it also introduces own |>
+
 
 # A map function is one that applies the same action/function to every element of an object 
 # (e.g. each entry of a list or a vector, or each of the columns of a data frame).
 
-addTen <- function(.x) {
-  return(.x + 10)
+add5percent <- function(.x) {
+  return((.x * 0.05) + .x)  #reusing args
+  #return((.x * 1.05) )
 }
 
-map(.x = c(1, 4, 7), 
-    .f = addTen)
+map(.x = c(1, 4, 7, 200), 
+    .f = add5percent)
 
-map(c(1, 4, 7), addTen)
+map(my_list[4], add5percent) #complete df
+map(my_list[[4]]$A, add5percent) # variable from df
 
-map(data.frame(a = 1, b = 4, c = 7), addTen)
+map_dbl(my_list[[5]]$mpg, add5percent) # double map on double vector
+map(my_list[[5]]$mpg, add5percent) # normal map on double vector
 
-map_dbl(c(1, 4, 7), addTen)
-# or short-hand with tilde and dot
-map_dbl(c(1, 4, 7), ~{.x + 10})
+
+
+
+
 
 map_chr(c(1, 4, 7), addTen)
 
