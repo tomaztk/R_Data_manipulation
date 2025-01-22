674b43c848d3ddcd80a8f9ac5625e9ee6aa850e8
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2025-01-22 22:32:17 +0100

    update

--- Session04/400_1_ggplot.R
+++ Session04/400_1_ggplot.R
@@ -80,3 +80,158 @@ ggplot(aes(x = displ, y = after_stat(count))) +
     geom_histogram()
 
 
+
+# aligning scales
+
+mpg_99 <- mpg2 %>% filter(year == 1999)
+mpg_08 <- mpg2 %>% filter(year == 2008)
+
+base_99 <- ggplot(mpg_99, aes(displ, hwy)) + geom_point() 
+base_08 <- ggplot(mpg_08, aes(displ, hwy)) + geom_point() 
+
+# we see that the scales are off
+base_99 #y-scale from 15 to 45
+base_08 #y-scale from 15 to 35
+
+
+# we fix the x-axis and y-axis with limits
+base_99 + 
+  scale_x_continuous(limits = c(1, 7)) +
+  scale_y_continuous(limits = c(10, 45))
+
+base_08 + 
+  scale_x_continuous(limits = c(1, 7)) +
+  scale_y_continuous(limits = c(10, 45))
+
+
+
+# adding breaks in scales
+
+break_data <- data.frame(
+  const = 1, 
+  up = 1:4,
+  txt = letters[1:4], 
+  big = (1:4)*1000,
+  log = c(2, 5, 10, 2000)
+)
+
+base <- ggplot(break_data, aes(big, const)) + 
+  geom_point() + 
+  labs(x = NULL, y = NULL) +
+  scale_y_continuous(breaks = NULL) 
+
+base
+
+
+# we can now change the breaks by adding major or minor gridlines
+
+base + scale_x_continuous(breaks = c(1000, 2000, 4000))
+base + scale_x_continuous(breaks = c(1000, 1500, 2000, 4000))
+
+
+# many different breaks
+
+#scales::breaks_extended() creates automatic breaks for numeric axes.
+#scales::breaks_log() creates breaks appropriate for log axes.
+#scales::breaks_pretty() creates “pretty” breaks for date/times.
+#scales::breaks_width() creates equally spaced breaks.
+
+
+#minor breaks
+
+base <- ggplot(break_data, aes(log, const)) + 
+  geom_point() + 
+  labs(x = NULL, y = NULL) +
+  scale_y_continuous(breaks = NULL) 
+
+# create x-scale
+mb <- unique(as.numeric(1:10 %o% 10 ^ (0:3)))
+
+base + scale_x_log10()
+base + scale_x_log10(minor_breaks = mb)
+
+
+# Labels
+
+base <- ggplot(break_data, aes(big, const)) + 
+  geom_point() + 
+  labs(x = NULL, y = NULL) +
+  scale_y_continuous(breaks = NULL) 
+
+base
+base + 
+  scale_x_continuous(
+    breaks = c(2000, 4000), 
+    labels = c("2k", "4k")
+  ) 
+
+
+base <- ggplot(break_data, aes(big, const)) + 
+  geom_point() + 
+  labs(x = NULL, y = NULL) +
+  scale_x_continuous(breaks = NULL)
+
+base
+
+base + scale_y_continuous(labels = scales::label_percent())
+
+base + scale_y_continuous(
+  labels = scales::label_dollar(prefix = "", suffix = "€")
+)
+
+
+## Transformations
+
+base <- ggplot(mpg2, aes(displ, hwy)) + geom_point()
+
+base
+base + scale_x_reverse()
+base + scale_y_reverse()
+
+# Every continuous scale takes a trans argument, allowing the use of a variety of transformations: 
+
+# convert from fuel economy to fuel consumption
+ggplot(mpg, aes(displ, hwy)) + 
+  geom_point() + 
+  scale_y_continuous(trans = "reciprocal")
+
+# log transform x and y axes
+ggplot(diamonds, aes(price, carat)) + 
+  geom_bin2d() + 
+  scale_x_continuous(trans = "log10") +
+  scale_y_continuous(trans = "log10")
+
+# couple of transformers
+# "log" 	scales::log_trans() 	
+# log10" 	scales::log10_trans()
+# "logit" 	scales::logit_trans()
+# "exp" 	scales::exp_trans()
+
+
+# same transformation
+mpg2 %>%
+ggplot(aes(displ, hwy)) + 
+  geom_point() + 
+  scale_y_continuous(trans = "reciprocal")
+
+mpg2 %>%
+ggplot(aes(displ, hwy)) + 
+  geom_point() + 
+  scale_y_continuous(trans = scales::reciprocal_trans())
+
+
+# or
+
+# manual transformation
+mpg2 %>%
+ggplot( aes(log10(displ), hwy)) + 
+  geom_point()
+
+# transform using scales
+mpg2 %>%
+ggplot( aes(displ, hwy)) + 
+  geom_point() + 
+  scale_x_log10()
+
+
+
